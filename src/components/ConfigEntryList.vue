95a6f275e573b4dea87a9afcdd76cba97df3fed5
deBabbbe <neotbh@gmx.de>
2022-06-10 20:30:26 +0200

    Entries can be removed

--- src/components/ConfigEntryList.vue
+++ src/components/ConfigEntryList.vue
@@ -2,16 +2,20 @@
   <div v-for="entry in currentConfigEntries" style="margin: 0 0 10px 10px">
     <input v-model="entry.key" style="margin-right: 5px" />
     <input v-model="entry.value" />
+    <button @click="removeEntry(entry)">Remove</button>
   </div>
 </template>
 
 <script lang="ts" setup>
 import type { IConfigurationEntry } from "@/data/configurationEntries";
-import { computed, ref } from "vue";
+import { computed } from "vue";
 
 const props = defineProps<{
   configEntries: IConfigurationEntry[];
 }>();
 
 const currentConfigEntries = computed(() => props.configEntries);
+const removeEntry = (entry: IConfigurationEntry) => {
+  props.configEntries.splice(props.configEntries.indexOf(entry), 1);
+};
 </script>
