1c8e69dc7c5c0f14a3a95b1eb7f7dadf3889bb96
deBabbbe <neotbh@gmx.de>
2022-06-10 22:37:48 +0200

    Added font awsome icons

--- src/components/ActionBar.vue
+++ src/components/ActionBar.vue
@@ -1,5 +1,42 @@
 <template>
-  <div className="ActionBar">Configurator</div>
+  <div className="ActionBar">
+    Configurator
+    <span>
+      <font-awesome-icon
+        icon="fa-rotate-right"
+        class="action-bar-button"
+        title="Add entry"
+      />
+    </span>
+    <span>
+      <font-awesome-icon
+        icon="fa-floppy-disk"
+        class="action-bar-button"
+        title="Add entry"
+      />
+    </span>
+    <span>
+      <font-awesome-icon
+        icon="fa-folder-open"
+        class="action-bar-button"
+        title="Add entry"
+      />
+    </span>
+    <span>
+      <font-awesome-icon
+        icon="fa-square-plus"
+        class="action-bar-button"
+        title="Add entry"
+      />
+    </span>
+    <span>
+      <font-awesome-icon
+        icon="fa-eye-slash"
+        class="action-bar-button"
+        title="Add entry"
+      />
+    </span>
+  </div>
 </template>
 <style>
 .ActionBar {
@@ -8,5 +45,9 @@
   color: white;
   padding: 10px;
   user-select: none;
+}
+.action-bar-button {
+  margin-right: 15px;
+  float: right;
 }
 </style>
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,22 @@
 import { createApp } from "vue";
+
 import App from "./App.vue";
 
-createApp(App).mount("#app");
+import { library } from "@fortawesome/fontawesome-svg-core";
+import {
+  faEyeSlash,
+  faFloppyDisk,
+  faFolderOpen,
+  faRotateRight,
+  faSquarePlus,
+} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+library.add(faSquarePlus);
+library.add(faRotateRight);
+library.add(faFloppyDisk);
+library.add(faFolderOpen);
+library.add(faEyeSlash);
+
+const app = createApp(App);
+app.component("font-awesome-icon", FontAwesomeIcon);
+app.mount("#app");
